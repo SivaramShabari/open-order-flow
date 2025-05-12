20ad76e13314fae11789e238ed2d1c3264f9acc4
Sivaram Shabari <me@sivaramshabari.me>
2025-05-13 04:26:32 +0530

    :rocket: :tada: Add all microservices templates and spring boot projects. Add user-service classes and entities

---
+++ services/payment-service/src/test/java/com/openorderflow/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openorderflow.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
